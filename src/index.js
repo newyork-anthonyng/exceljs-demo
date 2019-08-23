c8b2388c1a90493ccd77ee3f32689f0065b7d22c
Anthony Ng <newyork.anthonyng@gmail.com>
2019-08-23 10:11:06 -0400

    Add basic excel transform

--- src/index.js
+++ src/index.js
@@ -13,14 +13,57 @@ $dropZone.addEventListener("drop", function dropHandler(e) {
       const currentItem = e.dataTransfer.items[i];
       const file = currentItem.getAsFile();
       console.log(`file: ${i}`, file);
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = () => {
+        const buffer = reader.result;
+        const wb = new Excel.Workbook();
+        wb.xlsx.load(buffer).then(workbook => {
+          console.log(workbook, 'workbook instance');
+          workbook.eachSheet((sheet, id) => {
+            sheet.eachRow(function transformRow(row, rowIndex) {
+              const [
+                _,
+                orderDate,
+                region,
+                rep,
+                item,
+                units,
+                unitCost,
+                total
+              ] = row.values;
+
+              row.values = [
+                orderDate, region, rep, units, unitCost, item, total
+              ];
+            });
+          });
+          // workbook.eachSheet((sheet, id) => {
+          //   sheet.eachRow((row, rowIndex) => {
+          //     console.log(row.values, rowIndex);
+          //   })
+          // });
+          // const sheet = workbook.addWorksheet('Test sheet', {
+          //   properties: {
+          //     tabColor: {
+          //       argb:'FFC0000'
+          //     }
+          //   }
+          // });
+          // const row = sheet.addRow(['a', 'b', 'c']);
+          // row.font = { bold: true };
+
+          workbook.xlsx.writeBuffer().then(buffer => {
+            saveAs(new Blob([buffer]), 'abc.xlsx');
+          });
+        });
+      };
     }
   }
 });
 $dropZone.addEventListener("dragover", function(e) {
   e.preventDefault();
 });
-
-
 
 // Generate a new Excel file for download
 (async() => {
