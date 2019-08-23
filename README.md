817443332e0fd0a1fc83313cf084f2e9af8b0e73
Anthony Ng <newyork.anthonyng@gmail.com>
2019-08-23 09:49:48 -0400

    Add basic drag and drop

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exceljs Demo
+
+This demo shows how you can manipulate Excel sheets in the browser.
+
+## Getting started
+```shell
+yarn install
+
+yarn dev
+```
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import Excel from 'exceljs/dist/es5/exceljs.browser';
 import { saveAs } from 'file-saver'
 
+// Set up drag-and-drop functionality
+const $dropZone = document.querySelector('.js-drop');
+console.log($dropZone);
+$dropZone.addEventListener("drop", function dropHandler(e) {
+  console.log("Something was dropped");
+  e.preventDefault();
+
+  if (e.dataTransfer.items) {
+    for (let i = 0; i < e.dataTransfer.items.length; i++) {
+      const currentItem = e.dataTransfer.items[i];
+      const file = currentItem.getAsFile();
+      console.log(`file: ${i}`, file);
+    }
+  }
+});
+$dropZone.addEventListener("dragover", function(e) {
+  e.preventDefault();
+});
+
+
+
+// Generate a new Excel file for download
 (async() => {
   const workbook = new Excel.Workbook();
   const sheet = workbook.addWorksheet('Test sheet', {
@@ -14,5 +36,5 @@ import { saveAs } from 'file-saver'
   row.font = { bold: true };
 
   const buffer = await workbook.xlsx.writeBuffer();
-  saveAs(new Blob([buffer]), 'abc.xlsx');
+  // saveAs(new Blob([buffer]), 'abc.xlsx');
 })();
